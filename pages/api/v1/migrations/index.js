36cf26208f5583ecaa0fb1a42a004fad4e0480f8
Iago Machado <38327929+iagomachadocs@users.noreply.github.com>
2024-04-06 16:15:15 +0000

    adds `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,39 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    direction: "up",
+    dir: join("infra", "migrations"),
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405);
+}
