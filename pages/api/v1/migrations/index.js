90e18407f8d609f5616819d3a24d932184bc968a
Iago Machado <38327929+iagomachadocs@users.noreply.github.com>
2024-05-01 18:26:13 +0000

    refactoring

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,18 +3,26 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
 
-  const defaultOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    direction: "up",
-    dir: join("infra", "migrations"),
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
+  let dbClient;
   try {
+    dbClient = await database.getNewClient();
+
+    const defaultOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      direction: "up",
+      dir: join("infra", "migrations"),
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultOptions);
       return response.status(200).json(pendingMigrations);
@@ -32,8 +40,6 @@ export default async function migrations(request, response) {
 
       return response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).send();
   } catch (error) {
     console.error(error);
     throw error;
