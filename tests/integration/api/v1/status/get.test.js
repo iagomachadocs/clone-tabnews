0a6edca355185b4b883162ac483819f67eb077dd
Iago Machado <38327929+iagomachadocs@users.noreply.github.com>
2024-07-09 18:33:21 +0000

    adds `Automated Tests` GitHub Action

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET to /api/v1/status should return status code 200", async () => {
   const parsedUpdatedAt = new Date(updatedAt).toISOString();
   expect(parsedUpdatedAt).toBe(updatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("20.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.active_connections).toEqual(1);
 });
