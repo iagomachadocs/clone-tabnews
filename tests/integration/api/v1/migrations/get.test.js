ea624f9887c393520169ce6ee63bbfdcfd4bfe5d
Iago Machado <38327929+iagomachadocs@users.noreply.github.com>
2024-11-16 20:34:02 +0000

    chore: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,17 +6,16 @@ beforeAll(async () => {
   await database.clearDatabase();
 });
 
-test("GET to /api/v1/migrations should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const statusResponse = await fetch("http://localhost:3000/api/v1/status");
-  const statusBody = await statusResponse.json();
-
-  expect(statusBody.dependencies.database.active_connections).toBe(1);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const updatedAt = responseBody.updated_at;
-  const parsedUpdatedAt = new Date(updatedAt).toISOString();
-  expect(parsedUpdatedAt).toBe(updatedAt);
+      const updatedAt = responseBody.updated_at;
+      const parsedUpdatedAt = new Date(updatedAt).toISOString();
+      expect(parsedUpdatedAt).toBe(updatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.active_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.active_connections).toEqual(1);
+    });
+  });
 });
